263b195f846414b7f8c91d6aa6715ab4944299e6
Martin Milius <martin.milius@gmail.com>
2019-08-17 16:10:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  array.select{ 
-  array.reject
+  cals.any? do |word| word.length > 4
 end
 
 def find_the_cheese# code an argument here
